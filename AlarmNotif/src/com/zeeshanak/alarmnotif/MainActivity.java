1ed8fd59b02176f429e833d0d3ca63d0bd2b6c7d
ZeeshanAK <zeeshankhan.a1@gmail.com>
2015-05-11 01:50:17 +0500

    Implemented timePicker for start time

--- AlarmNotif/src/com/zeeshanak/alarmnotif/MainActivity.java
+++ AlarmNotif/src/com/zeeshanak/alarmnotif/MainActivity.java
@@ -4,11 +4,15 @@ import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +20,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -23,31 +28,21 @@ public class MainActivity extends Activity {
 	private PendingIntent alarmIntent;
 	private AudioManager ringer;
 	
-	
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		//Displaying current Ringer mode on the phone
-		ringer = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-		TextView textRinger = (TextView) findViewById(R.id.getRinger);
-		int currentRinger = ringer.getRingerMode();		
-		switch (currentRinger)
-		{
-			case 0:
-				textRinger.setText("Silent");
-				break;
-			case 1:
-				textRinger.setText("Vibrate");
-				break;
-			case 2:
-				textRinger.setText("Normal");
-				break;
-				default:
-					textRinger.setText("Cannot read ringer mode from settings!");
-		}
+		ringerMode();
+
+
+		final EditText startTime = (EditText) findViewById(R.id.startTime);
+		final EditText endTime = (EditText) findViewById(R.id.endTime);
+		//Populate the start and end time fields with current time
+		populateFields(startTime, endTime);
 		
 		
 		
@@ -58,13 +53,12 @@ public class MainActivity extends Activity {
 
 		
 		Button setButton = (Button) findViewById(R.id.setAlarm);
-		setButton.setOnClickListener(new OnClickListener() {
-			
+		setButton.setOnClickListener(new OnClickListener() 
+		{
+
 			@Override
 			public void onClick(View v) {
 				
-				EditText startTime = (EditText) findViewById(R.id.takeStartTime);
-				EditText endTime = (EditText) findViewById(R.id.takeEndTime);
 				
 				int start = Integer.decode(startTime.getText().toString());
 				int end = Integer.decode(endTime.getText().toString());
@@ -79,9 +73,47 @@ public class MainActivity extends Activity {
 		        Toast.makeText(MainActivity.this, "Alarm Set", Toast.LENGTH_SHORT).show();				
 			}
 		});
+
 	}
 	
-
+	public void showTimePickerDialog(View v) {
+	    DialogFragment newFragment = new TimePickerFragment();
+	    newFragment.show(getFragmentManager(), "timePicker");
+	}
+	
+	
+	public void ringerMode ()
+	{
+		ringer = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		TextView textRinger = (TextView) findViewById(R.id.getRinger);
+		int currentRinger = ringer.getRingerMode();		
+		switch (currentRinger)
+		{
+			case 0: textRinger.setText("Silent");
+				break;
+			case 1: textRinger.setText("Vibrate");
+				break;
+			case 2: textRinger.setText("Normal");
+				break;
+			default: textRinger.setText("Cannot read ringer mode from settings!");
+		}
+	}
+	
+	public void populateFields(View startTime, View endTime)
+	{
+		
+		Calendar mCal = Calendar.getInstance();
+		int hr = mCal.get(Calendar.HOUR_OF_DAY);
+		int mnt = mCal.get(Calendar.MINUTE);
+		int ap = mCal.get(Calendar.AM);
+		
+		((TextView) startTime).setText(String.valueOf(hr) + ": " + String.valueOf(mnt) 
+				+ (ap == 0 ? " AM": " PM"));
+		
+		((TextView) endTime).setText(String.valueOf(hr) + ": " + String.valueOf(mnt + 5) 
+				+ (ap == 0 ? " AM": " PM"));
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -102,7 +134,29 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 	
-//	
-//	public int getStart() {return start;}
-//	public int getEnd() { return end;}
+	//Class to implement timePicker dialog fragment
+	public class TimePickerFragment extends DialogFragment
+			    implements TimePickerDialog.OnTimeSetListener 
+	{
+			
+			@Override
+			public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
+		//	int amPM = c.get(Calendar.AM_PM);
+			
+				// Create a new instance of TimePickerDialog and return it
+				return new TimePickerDialog(getActivity(), this, hour, minute,
+				DateFormat.is24HourFormat(getActivity()));
+				}	
+			
+				public void onTimeSet(TimePicker view, int hourOfDay, int minute) 
+				{
+					final EditText startTime = (EditText) findViewById(R.id.startTime);
+
+					((TextView) startTime).setText(String.valueOf(hourOfDay) + ": " + String.valueOf(minute));
+				}
+	}
 }
