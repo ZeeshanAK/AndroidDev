8b01834a72ff17250b366ca100b585f69416c80c
ZeeshanAK <zeeshankhan.a1@gmail.com>
2015-05-12 21:02:09 +0500

    Added spinner and changed few things here and there.

--- AlarmNotif/src/com/zeeshanak/alarmnotif/MainActivity.java
+++ AlarmNotif/src/com/zeeshanak/alarmnotif/MainActivity.java
@@ -17,8 +17,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -28,60 +30,31 @@ public class MainActivity extends Activity {
 	private PendingIntent alarmIntent;
 	private AudioManager ringer;
 	private static String whichTextView;
+	private EditText startTime;
+	private EditText endTime; 
+	private Button setButton;
+	 
 	
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//Get required views
+		startTime = (EditText) findViewById(R.id.startTime);
+		endTime = (EditText) findViewById(R.id.endTime);
+		setButton = (Button) findViewById(R.id.setAlarm);
+
 		//Displaying current Ringer mode on the phone
 		ringerMode();
 
 		//Populate the start and end time fields with current time
-		final EditText startTime = (EditText) findViewById(R.id.startTime);
-		final EditText endTime = (EditText) findViewById(R.id.endTime);
 		populateFields(startTime, endTime);
 		
-		
-		
-		alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-		Intent intent = new Intent(MainActivity.this, AlarmReciever.class);
-		alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
-
-		
-		Button setButton = (Button) findViewById(R.id.setAlarm);
-		setButton.setOnClickListener(new OnClickListener() 
+		//Launch the Time Picker dialog when user clicks the Start Time field
+		startTime.setOnClickListener(new OnClickListener()
 		{
-
-			@Override
-			public void onClick(View v) {
-				
-				String startTextString = startTime.getText().toString();
-				String[] time = startTextString.split(":");
-				String hrStartString = time[0];
-				String mnStartStringAM = time[1];
-				String[] mnStartStringWithoutAM = mnStartStringAM.split(" ");
-				String mnStartString = mnStartStringWithoutAM[0];
-				
-				int startHR = Integer.parseInt(hrStartString);
-				int startMNT = Integer.parseInt(mnStartString);
-
-				 //Set the alarm
-				Calendar calendar = Calendar.getInstance();
-				calendar.setTimeInMillis(System.currentTimeMillis());
-				calendar.set(Calendar.HOUR_OF_DAY, startHR);
-				calendar.set(Calendar.MINUTE, startMNT);
-				
-				alarmMgr.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
-		        Toast.makeText(MainActivity.this, "Alarm Set " + mnStartString, Toast.LENGTH_LONG).show();				
-			}
-		});
-
-		
-		startTime.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) 
 			{
@@ -91,8 +64,9 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		endTime.setOnClickListener(new OnClickListener() {
-			
+		//Launch the Time Picker dialog when user clicks the End Time field
+		endTime.setOnClickListener(new OnClickListener() 
+		{			
 			@Override
 			public void onClick(View v) 
 			{
@@ -101,6 +75,28 @@ public class MainActivity extends Activity {
 			    newFragment.show(getFragmentManager(), "timePickerStart");					
 			}
 		});
+		
+		//Set the alarm when user clicks the Set Alarm button
+		setButton.setOnClickListener(new OnClickListener() 
+		{
+	
+			@Override
+			public void onClick(View v) 
+			{
+				setStartTime();
+			}
+		});
+		
+		
+		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.ringer_modes, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		
 		
 	}
 	
@@ -122,6 +118,7 @@ public class MainActivity extends Activity {
 			default: textRinger.setText("Cannot read ringer mode from settings!");
 		}
 	}
+	
 	
 	public void populateFields(View startTime, View endTime)
 	{
@@ -148,6 +145,7 @@ public class MainActivity extends Activity {
 				+amPM);
 	}
 	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -155,6 +153,7 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -166,6 +165,36 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	public void setStartTime()
+	{
+		alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+		Intent intent = new Intent(MainActivity.this, AlarmReciever.class);
+		alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+		//We receive the string as "12:12 AM", so splitting it to get the HR first 
+		String startTextString = startTime.getText().toString();
+		String[] getHR = startTextString.split(":");
+		String hrStartString = getHR[0];
+		String mnStartStringAM = getHR[1];
+		
+		//Split the second portion to drop the AM from the second part
+		String[] getMN = mnStartStringAM.split(" ");
+		String mnStartString = getMN[0];
+		
+		int startHR = Integer.parseInt(hrStartString);
+		int startMNT = Integer.parseInt(mnStartString);
+
+		 //Set the alarm
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		calendar.set(Calendar.HOUR_OF_DAY, startHR);
+		calendar.set(Calendar.MINUTE, startMNT);
+		
+		alarmMgr.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
+        Toast.makeText(MainActivity.this, "Alarm Set " + mnStartString, Toast.LENGTH_LONG).show();	
 	}
 	
 	//Class to implement timePicker dialog fragment
