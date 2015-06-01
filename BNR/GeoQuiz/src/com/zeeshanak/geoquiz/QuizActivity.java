ea9a6823d3e328c4a0e88e67718c99df9afb98d3
ZeeshanAK <zeeshankhan.a1@gmail.com>
2015-06-02 02:54:41 +0500

    Adding GeoQuiz

--- BNR/GeoQuiz/src/com/zeeshanak/geoquiz/QuizActivity.java
+++ BNR/GeoQuiz/src/com/zeeshanak/geoquiz/QuizActivity.java
@@ -4,13 +4,93 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class QuizActivity extends Activity {
+public class QuizActivity extends Activity 
+{
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]
+			{
+				new TrueFalse(R.string.question_oceans, true),
+				new TrueFalse(R.string.question_mideast, false),
+				new TrueFalse(R.string.question_africa, false),
+				new TrueFalse(R.string.question_americas, true),
+				new TrueFalse(R.string.question_asia, true),
+				new TrueFalse(R.string.question_mount, false),
+				new TrueFalse(R.string.question_obama, false)
+				
+			};
+	
+	private int mCurrentIndex =0;
+	
+	private void updateQuestion()
+	{
+		mQuestionTextView = (TextView)findViewById(R.id.questionTextView);
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);	
+	}
+	
+	private void checkAnswer(boolean userPressedTrue)
+	{
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		int messageResID = 0;
+		
+		if(userPressedTrue == answerIsTrue)
+		{
+			messageResID = R.string.correct_toast;
+		} else 
+		{
+			messageResID = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResID, Toast.LENGTH_LONG).show();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+		
+		updateQuestion();
+		
+		mNextButton = (Button) findViewById(R.id.nextButton);
+		mNextButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) 
+			{
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+		
+		mTrueButton = (Button) findViewById(R.id.trueButton);
+		mTrueButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				checkAnswer(true);
+			}
+		});
+		
+		mFalseButton = (Button) findViewById(R.id.falseButton);
+		mFalseButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
+			}
+		});
+		
+		
 	}
 
 	@Override
@@ -20,6 +100,8 @@ public class QuizActivity extends Activity {
 		return true;
 	}
 
+	
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
