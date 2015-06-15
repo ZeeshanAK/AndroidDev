44f607f2620c2e993c69b55a1319c335fd028274
ZeeshanAK <zeeshankhan.a1@gmail.com>
2015-06-15 21:36:18 +0500

    Bla1

--- BNR/GeoQuiz/src/com/zeeshanak/geoquiz/CheatActivity.java
+++ BNR/GeoQuiz/src/com/zeeshanak/geoquiz/CheatActivity.java
@@ -1,6 +1,7 @@
 package com.zeeshanak.geoquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,16 +11,29 @@ public class CheatActivity extends Activity
 {
 	
 	public static final String EXTRA_ANSWER_IS_TRUE =
-			"comz.zeeshanak.geoquiz.answer_is_true";
+			"com.zeeshanak.geoquiz.answer_is_true";
+	public static final String ANSWER_IS_SHOWN =
+			"com.zeeshanak.geoquiz.answer_is_shown";
+	
 	private boolean mAnswerIsTrue;
 	private Button mShowAnswer;
 	private TextView mAnswerTextView;
+	
+	
+	private void setAnswerisShown (boolean isAnswerShown)
+	{
+		Intent data = new Intent();
+		data.putExtra(ANSWER_IS_SHOWN, isAnswerShown);
+		setResult(RESULT_OK, data);
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cheat);
+		
+		setAnswerisShown(false);
 		
 		mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 		
@@ -36,6 +50,7 @@ public class CheatActivity extends Activity
 				{
 					mAnswerTextView.setText(R.string.false_button);
 				}
+				setAnswerisShown(true);
 			}
 		});
 	}
